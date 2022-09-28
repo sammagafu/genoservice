b57df69f3c5d6c70d6c19ce3baf1b52447c64737
Samuel Magafu <hi@sammagafu.com>
2022-09-28 15:49:22 +0300

    the updates

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,20 +1,20 @@
 <template>
-  <div class="px-[12px] md:px-[36px] xl:px-0 mt-[70px]">
+  <div class="px-[12px] md:px-[24px] xl:px-0 mt-[70px]">
     <div class="text-center mb-[35px] md:mb-[43px]">
       <h2
-        class="font-bold font-chivo mx-auto text-[35px] leading-[44px] md:text-[46px] md:leading-[52px] lg:text-heading-1 text-gray-900 mb-5 md:mb-[30px] max-w-[725px]">
+        class="font-bold font-pop mx-auto text-[35px] leading-[44px] md:text-[46px] md:leading-[52px] lg:text-heading-1 text-gray-900 mb-5 md:mb-[30px] max-w-[725px]">
         Our Story</h2>
-      <p class="font-pop md:text-lead-lg text-gray-600 mx-auto max-w-[976px]">GENO Services Limited is a Tanzanian firm,
+      <p class="text-justify font-pop md:text-lead-lg text-gray-600 mx-auto max-w-[976px]">GENO Services Limited is a Tanzanian firm,
         registered under incorporation No 93125, since August 2012, with a focus on Human Resources Consulting Services.
         We work with clients to craft the most appropriate Human Resource: People, Policies, Process & systems solutions
         which integrates the current and future business needs as our customer’s organization grows.</p>
-      <p class="font-pop md:text-lead-lg text-gray-600 mx-auto max-w-[976px]">25+ years of Human Resource Experiences of
+      <p class="text-justify	font-pop md:text-lead-lg text-gray-600 mx-auto max-w-[976px]">25+ years of Human Resource Experiences of
         the Advisors, and Consultants together with Strategic Partnerships with other professional firms e.g. Clarita’s
         International (Audit and Accounting firm), Noesis Training Institutes and Breakthrough Consulting, GENO provide
         world-class services to all our clients.</p>
-      <p class="font-pop md:text-lead-lg text-gray-600 mx-auto max-w-[976px]">Our Advisors and Consultants are
+      <p class="text-justify	font-pop md:text-lead-lg text-gray-600 mx-auto max-w-[976px]">Our Advisors and Consultants are
         Experienced and Certified members of the globally recognized Professional Institutions.</p>
-      <p class="font-pop md:text-lead-lg text-gray-600 mx-auto max-w-[976px]">Our exceptional innovative approach to
+      <p class="text-justify	font-pop md:text-lead-lg text-gray-600 mx-auto max-w-[976px]">Our exceptional innovative approach to
         service delivery, distinguishes us, notwithstanding the diversity and complexity of our customer’s needs.</p>
     </div>
     <!-- <p class="font-medium font-pop text-[16px] leading-[26px] md:text-heading-4 text-gray-600 mx-auto mb-[40px] md:mb-[75px] md:w-[47ch]">GENO Services Limited is a Tanzanian firm, registered under incorporation No 93125, since August 2012, with a focus on Human Resources Consulting Services. We work with clients to craft the most appropriate Human Resource: People, Policies, Process & systems solutions which integrates the current and future business needs as our customer’s organization grows.</p> -->
@@ -51,7 +51,7 @@
         class="flex items-start gap-5 transition-all duration-300 mb-[33px] hover:translate-y-[-3px] border-b border-gray-200 pb-[50px] last:mb-0">
         <img class="h-full w-full object-cover max-w-[64px]" src="../assets/icons/astronaut.png" alt="icon">
         <div>
-          <h3 class="font-bold font-chivo text-[20px] leading-[26px] md:text-heading-4 mb-[14px]">
+          <h3 class="font-bold font-pop text-[20px] leading-[26px] md:text-heading-4 mb-[14px]">
             Professionalism
           </h3>
           <p class="text-excerpt">We are seasoned professionals, continuously educating ourselves and preparing for the
@@ -62,7 +62,7 @@
         class="flex items-start gap-5 transition-all duration-300 mb-[33px] hover:translate-y-[-3px] border-b border-gray-200 pb-[50px] last:mb-0">
         <img class="h-full w-full object-cover max-w-[64px]" src="../assets/icons/bulb.png" alt="icon">
         <div>
-          <h3 class="font-bold font-chivo text-[20px] leading-[26px] md:text-heading-4 mb-[14px]">Innovation</h3>
+          <h3 class="font-bold font-pop text-[20px] leading-[26px] md:text-heading-4 mb-[14px]">Innovation</h3>
           <p class="text-excerpt">We create an atmosphere where everyone’s idea counts. <br></p>
         </div>
       </div>
@@ -70,7 +70,7 @@
         class="flex items-start gap-5 transition-all duration-300 mb-[33px] hover:translate-y-[-3px] border-b border-gray-200 pb-[50px]">
         <img class="h-full w-full object-cover max-w-[64px]" src="../assets/icons/integration.png" alt="icon">
         <div>
-          <h3 class="font-bold font-chivo text-[20px] leading-[26px] md:text-heading-4 mb-[14px]">Integrity
+          <h3 class="font-bold font-pop text-[20px] leading-[26px] md:text-heading-4 mb-[14px]">Integrity
           </h3>
           <p class="text-excerpt">We demonstrate integrity through being fair, honest and confidential.</p>
         </div>
@@ -79,7 +79,7 @@
         class="flex items-start gap-5 transition-all duration-300 mb-[33px] hover:translate-y-[-3px] border-b border-gray-200 pb-[50px]">
         <img class="h-full w-full object-cover max-w-[64px]" src="../assets/icons/medal.png" alt="icon">
         <div>
-          <h3 class="font-bold font-chivo text-[20px] leading-[26px] md:text-heading-4 mb-[14px]">Quality
+          <h3 class="font-bold font-pop text-[20px] leading-[26px] md:text-heading-4 mb-[14px]">Quality
           </h3>
           <p class="text-excerpt">We are a high performing, high quality organization dedicated to human resource
             services.</p>
